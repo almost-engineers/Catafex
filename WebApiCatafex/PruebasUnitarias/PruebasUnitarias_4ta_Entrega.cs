52d043067deb7399be8491e147156f8bcf335cfe
julian5147 <julisalgado71@gmail.com>
2020-03-13 01:02:53 -0500

    implementación pruebas unitarias para el método obtenerObservaciones

--- WebApiCatafex/PruebasUnitarias/PruebasUnitarias_4ta_Entrega.cs
+++ WebApiCatafex/PruebasUnitarias/PruebasUnitarias_4ta_Entrega.cs
@@ -118,7 +118,7 @@ namespace PruebasUnitarias
         }
 
         /// <summary>
-        /// Obtener las observaciones de un panel terminado 
+        /// Obtener las observaciones de un panel donde no se hayan terminados todas sus catas
         /// </summary>
         [TestMethod]
         public void ObtenerObservacionesPanelNoTerminado()
@@ -127,226 +127,28 @@ namespace PruebasUnitarias
             var response = this.apiObtenerReporte.obtenerObservaciones(codPanel);
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadRequest);
         }
-        /// <summary>
-        ///  Realizar una autenticación con un correo no registrado en el sistema.
-        /// </summary>
-       /* [TestMethod]
-        public void autenticarCatadorCorreoFallido()
-        {
-            Catador catador = new Catador()
-            {
-                correo = "jhonathan59@catafex.com",
-                contrasena = "123456"
-            };
-            var response = apiAutenticar.validarCamposCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-        /// <summary>
-        /// metodo test validar realizar una autenticación con una contraseña incorrecta.
-        /// </summary>
-        [TestMethod]
-        public void autenticarCatadorContraseñaFallida()
-        {
-            Catador catador = new Catador()
-            {
-                correo = "jhonatha3n@catafex.com",
-                contrasena = "1234567"
-            };
-            var response = apiAutenticar.validarCamposCatador(catador);
-            Assert.AreEqual(System.Net.HttpStatusCode.BadGateway, response.StatusCode);
-        }
-        /// <summary>
-        /// metodo test para Registrar correctamente los datos de un café para una cata.
-        /// </summary>
-        [TestMethod]
-        public void registrarCataCorrecta()
-        {
-            Cata cata = new Cata("CATA-01", 1, 7, 9, 4, 6, 2, 8, 5, 7, 8, "Excelente Cafe");
-            var response = apiCata.registrarCata(cata);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
-        }
+
 
         /// <summary>
-        /// metodo test para Registrar los datos de una cata con valores negativos.
+        /// Intentar Obtener las observaciones de un panel donde su código no sea correcto 
         /// </summary>
         [TestMethod]
-        public void registrarCataFallida()
+        public void ObtenerObservacionesCódigoPanelInvalido()
         {
-            Cata cata = new Cata("CATA-01", -1, 7, 9, 4, 6, 2, 8, 5, 11, 8, "Excelente Cafe");
-            var response = apiCata.registrarCata(cata);
+            string codPanel = "codPanel";
+            var response = this.apiObtenerReporte.obtenerObservaciones(codPanel);
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadRequest);
         }
 
         /// <summary>
-        /// metodo test para Consultar catas pendientes, igual a las catas en la base de datos
+        /// Intentar Obtener el gráfico en cuál el código del panel este vacío
         /// </summary>
         [TestMethod]
-        public void obtenerCatasPendientes()
+        public void ObtenerObservacionesCódigoPanelVacio()
         {
-            var response = apiCata.consultarCatacion("CATADOR001");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            string codPanel = "";
+            var response = this.apiObtenerReporte.obtenerObservaciones(codPanel);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadRequest);
         }
-
-        /// <summary>
-        /// metodo test para Consultar catas pendientes, sin catas pendientes, todas ya han sido realizadas
-        /// </summary>
-        [TestMethod]
-        public void sinCatasPendientes()
-        {
-            var response = apiCata.consultarCatacion("CATADOR002");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-
-        /// <summary>
-        /// metodo test para  registrar un catador valido
-        /// </summary>
-        [TestMethod]
-        public void registrarCatadorExitoso()
-        {
-            Catador catador = new Catador()
-            {
-                nombre = "Jhonathan",
-                cedula = "9999",
-                correo = "jhonathan2@catafex.com",
-                codigo = "CATADOR002",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "1234"
-            };
-            var response = apiRegistrar.insertarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
-        }
-
-        /// <summary>
-        /// metodo test para llenar un catador sin llenar el campo de nombre 
-        /// </summary>
-        [TestMethod]
-        public void registrarCatadorSinNombre()
-        {
-            Catador catador = new Catador()
-            {
-                cedula = "9999",
-                correo = "jhonathan2@catafex.com",
-                codigo = "CATADOR002",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "1234"
-            };
-            var response = apiRegistrar.insertarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.NotFound);
-        }
-
-        /// <summary>
-        /// registrar un catador con la cedula ya existente
-        /// </summary>
-        [TestMethod]
-        public void registrarCatadorCedulaRepetida()
-        {
-            Catador catador = new Catador()
-            {
-                cedula = "1234",
-                correo = "jhonathan2@catafex.com",
-                codigo = "CATADOR002",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "1234"
-            };
-            var response = apiRegistrar.insertarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-
-        /// <summary>
-        /// metodo test para registar un catador con una cedula invalida (que no posea solo numeros)
-        /// </summary>
-        [TestMethod]
-        public void registrarCatadorCedulaInvalida()
-        {
-            Catador catador = new Catador()
-            {
-                cedula = "8243F",
-                correo = "jhonathan2@catafex.com",
-                codigo = "CATADOR002",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "1234"
-            };
-            var response = apiRegistrar.insertarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-
-        /// <summary>
-        /// metodo para probar la actualizacion de datos de un catador 
-        /// solo se pueden actualizar el nombre, correo y contraseña, pero se mandan 
-        /// los demas datos para probar el metodo(los demas deben coinidir)
-        /// </summary>
-        [TestMethod]
-        public void actualizarCatadorExitoso()
-        {
-            Catador catador = new Catador()
-            {
-                cedula = "1234",
-                nombre = "jhonathan",
-                correo = "jhonathan@catafex.com",
-                codigo = "CATADOR001",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "12345"
-            };
-            var response = apiRegistrar.actualizarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
-        }
-        /// <summary>
-        /// metodo test para actualizar en elk cual no se le manda un nombre 
-        /// 
-        /// </summary>
-        [TestMethod]
-        public void actualizarCatadorFallido()
-        {
-            Catador catador = new Catador()
-            {
-                nombre = "",
-                cedula = "1234",
-                correo = "jhonathan@catafex.com",
-                codigo = "CATADOR001",
-                nivelExp = "EXPERIMENTADO",
-                contrasena = "0752"
-            };
-            var response = apiRegistrar.actualizarCatador(catador);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-
-        /// <summary>
-        /// metodo test para eliminar un catador con su cedula 
-        /// </summary>
-        [TestMethod]
-        public void eliminarCatadorExitoso()
-        {
-            var response = apiRegistrar.eliminarCatador("1234");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
-        }
-        /// <summary>
-        /// metodo test para eliminar catador con una cedula no existente
-        /// </summary>
-        [TestMethod]
-        public void eliminarCatadorNoRegistrado()
-        {
-            var response = apiRegistrar.eliminarCatador("9999");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-        /// <summary>
-        /// metodo test para eliminar ctador con una cedula  invalida
-        /// </summary>
-        [TestMethod]
-        public void eliminarCatadorcedulaInvalida()
-        {
-            var response = apiRegistrar.eliminarCatador("123o4p");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }
-
-        /// <summary>
-        /// metodo test para eliminar un catador con la cedula vacia
-        /// </summary>
-        [TestMethod]
-        public void eliminarCatadorCedulaVacia()
-        {
-            var response = apiRegistrar.eliminarCatador("");
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.BadGateway);
-        }*/
-
     }
 }
